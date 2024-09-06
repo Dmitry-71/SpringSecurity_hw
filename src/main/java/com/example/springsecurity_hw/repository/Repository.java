debc6e663b0109f3dd3b2f080e52f5e865b3030a
Dmitry_71 <dgorodilov@mail.ru>
2024-09-06 13:08:55 +0300

    init commit

---
+++ src/main/java/com/example/springsecurity_hw/repository/Repository.java
@@ -0,0 +1,17 @@
+package com.example.springsecurity_hw.repository;
+
+import com.example.springsecurity_hw.entity.Person;
+import com.example.springsecurity_hw.entity.PersonId;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+@org.springframework.stereotype.Repository
+public interface Repository extends JpaRepository<Person, PersonId> {
+    List<Person> findByCityOfLiving(String city);
+
+    List<Person> findByAgeLessThanOrderByAge(int age);
+
+    Optional<Person> findByNameAndSurname(String name, String surname);
+}
